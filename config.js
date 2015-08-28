adb18630c5eb58babd262856b7d3cfc5d29af9dc
jackrobertscott <mast.j.scott@gmail.com>
2015-08-28 11:25:45 +0800

    update config file

--- config.js
+++ config.js
@@ -9,15 +9,18 @@ module.exports = {
 
     // App
     app: path.join(__dirname, 'app'),
-    controllers: path.join(__dirname, 'app/controllers'),
-    models: path.join(__dirname, 'app/models'),
-    views: path.join(__dirname, 'app/views'),
+    controllers: path.join(__dirname, 'app', 'controllers'),
+    models: path.join(__dirname, 'app', 'models'),
+    views: path.join(__dirname, 'app', 'views'),
 
     // Public
     public: path.join(__dirname, 'public'),
-    images: path.join(__dirname, 'public/images'),
-    javascripts: path.join(__dirname, 'public/javascripts'),
-    stylesheets: path.join(__dirname, 'public/stylesheets'),
+    images: path.join(__dirname, 'public', 'images'),
+    javascripts: path.join(__dirname, 'public', 'javascripts'),
+    stylesheets: path.join(__dirname, 'public', 'stylesheets'),
+
+    // Build
+    build: path.join(__dirname, 'build'),
 
     // Dependencies
     bower_components: path.join(__dirname, 'bower_components'),

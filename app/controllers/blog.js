2637a54b8b24746cf94750a5a7d99617e795246c
jackrobertscott <mast.j.scott@gmail.com>
2015-08-28 17:23:34 +0800

    add blog entity functionality

---
+++ app/controllers/blog.js
@@ -0,0 +1,35 @@
+var config = require('../../config');
+var express = require('express');
+var router = express.Router();
+var Post = require('../models/post');
+var _ = require('lodash');
+
+module.exports = function BlogController(app) {
+  app.use('/blog', router);
+};
+
+router.get('/', function(req, res, next) {
+  Post.findBySite(config.firebase.siteId, function(err, posts) {
+    if (err) return next(err);
+
+    res.render('blog', {
+      posts: posts
+    });
+  });
+});
+
+router.get('/:url', function(req, res, next) {
+  Post.findBySite(config.firebase.siteId, function(err, posts) {
+    if (err) return next(err);
+
+    var post = _.find(posts, function(post) {
+      return post.url === req.params.url;
+    });
+    if (!post) return next(err);
+
+    res.render('post', {
+      posts: posts,
+      post: post
+    });
+  });
+});
